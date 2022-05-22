41148bd6bb6c4dff3068252fbe6b20feaa9fc51b
CoreyEditBay <cortega72@cnm.edu>
2022-05-21 18:11:05 -0600

    Add project files.

---
+++ CTO_CreatingCS/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Programmer: Corey Ortega
+ * Email:      cortega72@cnm.edu
+ * date:       02/09/2022
+ * program:    Gravitation.cs
+ * purpose:    First quiz of the class - creating and using classes and methods
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTO_CreatingCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string yourName = "Corey Ortega";
+            Console.WriteLine("Hello! My name is "+yourName);
+        }
+    }
+}
